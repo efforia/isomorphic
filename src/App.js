b06504fdbfde915245306945d2ec90e33be7dc5a
William Oliveira de Lagos <william.lagos@live.com>
2017-10-28 00:38:42 -0200

    Added form component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      passw: '',
+      login: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    alert('Submitted!' + this.state.login + '  ' + this.state.passw);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const vals = event.target.value;
+    console.log({[name]:vals});
+    this.setState({ [name]: vals });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bem-vindo ao Frete Fácil</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <form className="form-signin" onSubmit={this.handleSubmit}>
+        <h2 className="form-signin-heading">Entre</h2>
+        <input type="email" name="login" className="form-control" value={this.state.login} onChange={this.handleChange} placeholder="E-mail"/>
+        <input type="password" name="passw" className="form-control" value={this.state.passw} onChange={this.handleChange} placeholder="Senha"/>
+        <button className="btn btn-lg btn-success btn-block" type="submit">Entrar</button>
+      </form>
     );
   }
 }
