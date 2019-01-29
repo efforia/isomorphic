55d1e0ff4d4bfd16e8decd357cccdec54aa5a9bf
William Oliveira de Lagos <william.lagos@live.com>
2019-01-29 19:33:31 -0200

    Updated react tools and added Ionic support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './components/app/App';
+import { registerIonic } from '@ionic/react';
 import registerServiceWorker from './registerServiceWorker';
 
+registerIonic();
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
