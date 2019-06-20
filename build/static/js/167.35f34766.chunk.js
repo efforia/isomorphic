3e0d53caa6ff423fd4200bf352520204fdef7f57
William Oliveira de Lagos <william.lagos@icloud.com>
2019-06-20 18:49:02 -0300

    Cleared source tree and set default font for application

--- build/static/js/167.35f34766.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([167],{981:function(n,t,e){"use strict";function o(n,t){n.addEventListener("statusTap",function(){t.read(function(){var e=n.document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(e){var o=e.closest("ion-content");o&&o.componentOnReady().then(function(){t.write(function(){return o.scrollToTop(300)})})}})})}Object.defineProperty(t,"__esModule",{value:!0}),e.d(t,"startStatusTap",function(){return o})}});
-//# sourceMappingURL=167.35f34766.chunk.js.map
